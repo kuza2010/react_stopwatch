c9082de8e34e6e67ce0c608260df6e97800b0240
Artyom <danilinartem1997@gmail.com>
2020-06-26 16:41:22 +0300

    Updates

---
+++ precache-manifest.fd5c141f52ba3cdd6646d5fd00505567.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f5813685db7b280e3f1824fc83fe153",
+    "url": "/react_stopwatch/index.html"
+  },
+  {
+    "revision": "61684b614bc92c7c5097",
+    "url": "/react_stopwatch/static/js/2.6516efa5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_stopwatch/static/js/2.6516efa5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6091d6239737fcf72bd9",
+    "url": "/react_stopwatch/static/js/main.f92c074f.chunk.js"
+  },
+  {
+    "revision": "c1ced05359986f999e2c",
+    "url": "/react_stopwatch/static/js/runtime-main.e4772b4e.js"
+  }
+]);
